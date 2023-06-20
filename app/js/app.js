8a820b2f0b58dc72fabbbc9950773fe0e38ffd9f
Syrone <cazonove@gmail.com>
2023-06-21 00:10:57 +0300

    page hanky code

---
+++ app/js/app.js
@@ -0,0 +1,187 @@
+// // Import vendor jQuery plugin example
+// import '~/app/libs/mmenu/dist/mmenu.js'
+
+document.addEventListener('DOMContentLoaded', () => {
+
+	//Swiper из второго блока
+	const swiperLeftAuto = new Swiper('.swiper-left-auto', {
+		a11y: false,
+		direction: 'horizontal',
+		loop: true,
+		speed: 4000,
+		spaceBetween: 50,
+		slidesPerView: 'auto',
+		freeMode: true,
+		autoplay: {
+			delay: 0,
+			disableOnInteraction: false,
+		},
+
+		noSwiping: true,
+		noSwipingClass: 'swiper-slide',
+	});
+
+	//Swiper из второго блока
+	const swiperLeftAuto2 = new Swiper('.swiper-left-auto2', {
+		a11y: false,
+		direction: 'horizontal',
+		loop: true,
+		speed: 7000,
+		spaceBetween: 50,
+		slidesPerView: 'auto',
+		freeMode: true,
+		autoplay: {
+			delay: 0.2,
+			disableOnInteraction: false,
+		},
+
+		noSwiping: true,
+		noSwipingClass: 'swiper-slide',
+	});
+
+	//Swiper из третьего блока
+	const swiperEffectCards = new Swiper(".swiper-effect-card", {
+		effect: "cards",
+		grabCursor: true,
+		slidesPerView: "auto",
+		centeredSlides: true,
+		noSwiping: true,
+		noSwipingClass: 'swiper-slide',
+
+		cardsEffect: {
+			perSlideOffset: 15,
+			rotate: false,
+			slideShadows: true,
+		},
+	});
+
+	//Swiper из третьего блока в popup окне
+	const swiperEffectCards2 = new Swiper(".swiper-effect-card2", {
+		grabCursor: true,
+		slidesPerView: 1,
+		spaceBetween: 50,
+
+		navigation: {
+			nextEl: '.swiper-effect-card2-next',
+			prevEl: '.swiper-effect-card2-prev',
+		},
+
+		scrollbar: {
+			el: '.swiper-effect-card2-scrollbar',
+			draggable: true,
+			dragSize: 'auto',
+		 },
+	});
+
+
+	// Переворот карточек swiper-left-auto и swiper-left-auto2
+	let slideItems = document.querySelectorAll('.slide-item');
+
+	if (slideItems) {
+		slideItems.forEach(slide => {
+			slide.addEventListener('click', function () {
+				this.classList.add('active');
+			});
+		});
+
+		let slideCloseButtons = document.querySelectorAll('.slide-item-close');
+
+		slideCloseButtons.forEach(button => {
+			button.addEventListener('click', function (event) {
+				event.stopPropagation();
+				let slide = this.closest('.slide-item');
+				if (slide) {
+					slide.classList.remove('active');
+				}
+			});
+		});
+	}
+
+	//Кастомный Select
+	class CustomSelect {
+		constructor(selectElement) {
+			this.selectElement = selectElement;
+			this.selectButton = selectElement.querySelector('.select-button');
+			this.selectContent = selectElement.querySelector('.select-content');
+			this.options = Array.from(selectElement.querySelectorAll('.options li'));
+
+			this.selectButton.addEventListener('click', () => this.toggleOptions());
+			this.options.forEach(option => {
+				option.addEventListener('click', () => this.selectOption(option));
+			});
+
+			document.addEventListener('click', (event) => this.closeOptions(event));
+		}
+
+		toggleOptions() {
+			this.selectElement.classList.toggle('active');
+		}
+
+		selectOption(option) {
+			const selectedText = option.textContent;
+			this.selectButton.querySelector('.select').textContent = selectedText;
+			this.toggleOptions();
+		}
+
+		closeOptions(event) {
+			if (!this.selectElement.contains(event.target)) {
+				this.selectElement.classList.remove('active');
+			}
+		}
+	}
+
+	const customSelects = Array.from(document.querySelectorAll('.custom-select'));
+	if (customSelects) {
+		customSelects.forEach(select => new CustomSelect(select));
+	}
+
+	// Получаем элементы списка
+	const selectOptions = document.querySelectorAll('#swiper-select-color .options li');
+
+	// Связывает Select и swiper выбором цвета
+	if (selectOptions) {
+		selectOptions.forEach((option, index) => {
+			option.addEventListener('click', () => {
+				swiperEffectCards.slideTo(index);
+			});
+		});
+	}
+
+	//Кастомный Popup
+	class Popup {
+		constructor(popupElement) {
+			this.popup = popupElement;
+			this.closeBtn = this.popup.querySelector('.custom-popup-close');
+			this.closeBtn.addEventListener('click', () => this.close());
+			window.addEventListener('click', (event) => {
+				if (event.target == this.popup) {
+					this.close();
+				}
+			});
+			window.addEventListener('keydown', (event) => {
+				if (event.key === 'Escape') {
+					this.close();
+				}
+			});
+		}
+
+		open() {
+			this.popup.classList.add('custom-popup-show');
+			document.body.classList.add('overflow-hidden');
+		}
+
+		close() {
+			this.popup.classList.remove('custom-popup-show');
+			document.body.classList.remove('overflow-hidden');
+		}
+	}
+
+	document.querySelectorAll('.open-popup').forEach(button => {
+		const popupId = button.dataset.popupTarget;
+		const popupElement = document.querySelector(popupId);
+		const popup = new Popup(popupElement);
+		button.addEventListener('click', () => popup.open());
+	});
+
+
+})
---
+++ app/js/main.js
@@ -0,0 +1,262 @@
+/* главная категории */
+    var swiper = new Swiper(".my-swiper-category", {
+		slidesPerView: 7,
+		slidesPerGroup: 1,
+      spaceBetween: 0,
+		loop: false,
+      centeredSlides: false,
+		navigation: {
+    nextEl: '.swiper-button-next-category',
+    prevEl: '.swiper-button-prev-category'
+  },
+      scrollbar: {
+        el: ".swiper-scrollbar",
+        hide: false,
+      },
+		breakpoints: {
+    0: {
+      slidesPerView: 2,
+      spaceBetween: 0,
+		slidesPerGroup: 1,
+    },
+			1024: {
+      slidesPerView: 7,
+      spaceBetween: 0,
+		slidesPerGroup: 1,
+    },
+  },
+    });
+	
+
+
+ /* главная Best-Selling   */
+
+
+    var swiper = new Swiper(".my-swiper-best-selling", {
+		slidesPerView: 4,
+		slidesPerGroup: 1,
+      spaceBetween: 0,
+		loop: false,
+      centeredSlides: false,
+		navigation: {
+    nextEl: '.swiper-button-next-best-selling',
+    prevEl: '.swiper-button-prev-best-selling'
+  },
+      scrollbar: {
+        el: ".swiper-scrollbar",
+        hide: false,
+      },
+		 breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+		slidesPerGroup: 1,
+    },
+			 			1024: {
+      slidesPerView: 4,
+      spaceBetween: 0,
+		slidesPerGroup: 1,
+    },
+  },
+    });
+
+/* главная Superstore */
+
+    var swiper = new Swiper(".my-swiper-superstore", {
+		slidesPerView: 1,
+		slidesPerGroup: 1,
+      spaceBetween: 0,
+		loop: false,
+      centeredSlides: false,
+		navigation: {
+    nextEl: '.swiper-button-next-superstore',
+    prevEl: '.swiper-button-prev-superstore'
+  },
+      scrollbar: {
+        el: ".swiper-scrollbar",
+        hide: false,
+      },
+			 breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+		slidesPerGroup: 1,
+    },
+
+  },
+    });
+
+/* главная The lastest */
+
+
+    var swiper = new Swiper(".my-swiper-lastest", {
+		slidesPerView: 4,
+		slidesPerGroup: 1,
+      spaceBetween: 0,
+		loop: false,
+      centeredSlides: false,
+		navigation: {
+    nextEl: '.swiper-button-next-lastest',
+    prevEl: '.swiper-button-prev-lastest'
+  },
+      scrollbar: {
+        el: ".swiper-scrollbar",
+        hide: false,
+      },
+			 breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+		slidesPerGroup: 1,
+    },
+				 			1024: {
+      slidesPerView: 4,
+      spaceBetween: 0,
+		slidesPerGroup: 1,
+    },
+  },
+    });
+
+
+/* главная Do more */
+
+
+    var swiper = new Swiper(".my-swiper-more", {
+		slidesPerView: 2,
+		slidesPerGroup: 1,
+      spaceBetween: 0,
+		loop: false,
+      centeredSlides: false,
+			navigation: {
+    nextEl: '.swiper-button-next-more',
+    prevEl: '.swiper-button-prev-more'
+  },
+      scrollbar: {
+        el: ".swiper-scrollbar",
+        hide: false,
+      },
+			 breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+		slidesPerGroup: 1,
+    },
+				 			1024: {
+      slidesPerView: 2,
+      spaceBetween: 0,
+		slidesPerGroup: 1,
+    },
+  },
+    });
+
+
+/* главная Shop by group */ 
+
+
+    var swiper = new Swiper(".my-swiper-group", {
+		slidesPerView: 3,
+		slidesPerGroup: 1,
+      spaceBetween: 0,
+		loop: false,
+      centeredSlides: false,
+			navigation: {
+    nextEl: '.swiper-button-next-group',
+    prevEl: '.swiper-button-prev-group'
+  },
+      scrollbar: {
+        el: ".swiper-scrollbar",
+        hide: false,
+      },
+			 breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+		slidesPerGroup: 1,
+    },
+				 			1024: {
+      slidesPerView: 3,
+      spaceBetween: 0,
+		slidesPerGroup: 1,
+    },
+  },
+    });
+
+
+
+/* gift card */
+
+
+    var swiper = new Swiper(".my-swiper-gift", {
+		slidesPerView: 1,
+		slidesPerGroup: 1,
+      spaceBetween: 0,
+		loop: true,
+      centeredSlides: false,
+			navigation: {
+    nextEl: '.swiper-button-next-gift',
+    prevEl: '.swiper-button-prev-gift'
+  },
+      scrollbar: {
+        el: ".swiper-scrollbar",
+        hide: false,
+      },
+		
+    });
+
+
+
+
+/* главная категории */
+
+
+    var swiper = new Swiper(".cell-dilivery", {
+		slidesPerView: 6,
+		slidesPerGroup: 1,
+      spaceBetween: 0,
+		loop: false,
+      centeredSlides: false,
+		scrollbar: {
+        el: ".swiper-scrollbar",
+        hide: false,
+      },
+      
+		breakpoints: {
+    0: {
+      slidesPerView: 2,
+      spaceBetween: 0,
+		slidesPerGroup: 1,
+    },
+			1024: {
+      slidesPerView: 6,
+      spaceBetween: 0,
+		slidesPerGroup: 1,
+    },
+  },
+    });
+
+
+
+
+/* Affiliate program */
+
+
+var swiper = new Swiper(".swiper-howwork", {
+    slidesPerView: 4,
+    slidesPerGroup: 1,
+	loop: false,
+    navigation: {
+        nextEl: ".swiper-button-next-how",
+        prevEl: ".swiper-button-prev-how",
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+            loop: true,
+        },
+        1024: {
+            slidesPerView: 4,
+        },
+    },
+});	
+                                
